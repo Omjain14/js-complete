64d1c2a8c355acf83bc10fbf78a0823017176ced
Omjain <omjain1401@gmail.com>
2026-01-04 23:55:12 +0530

    dom started

---
+++ js(part2)/dom.js
@@ -0,0 +1,5 @@
+//dom: Document Object Model
+
+// The DOM represents the page so that programs can change the document structure, style, and content.
+// The DOM represents the document as nodes and objects. That way, programming languages can connect to the page.
+// A web page is a document. This document can be either displayed in the browser window or as the HTML source code of the page.
