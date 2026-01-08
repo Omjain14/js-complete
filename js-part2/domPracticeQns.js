0ff67b9eafb37785f69448dbd18dbccfd0c0a137
Omjain <omjain1401@gmail.com>
2026-01-08 23:55:32 +0530

    js dom

--- js-part2/domPracticeQns.js
+++ js-part2/domPracticeQns.js
@@ -1,6 +1,6 @@
-//Task1 : Select the heading of a page by ID and change its text to "Welcome to the DOM Practice Page"
-let heading = document.querySelector("#header");
-heading.textContent ="Welcome to the DOM Practice Page";
+// //Task1 : Select the heading of a page by ID and change its text to "Welcome to the DOM Practice Page"
+// let heading = document.querySelector("#header");
+// heading.textContent ="Welcome to the DOM Practice Page";
 
 
 //Task 2: Select all <li> elrmrnts and print their text using a loop
